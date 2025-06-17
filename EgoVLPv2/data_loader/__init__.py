da39da9022b96375bcbea25fb59cf09fb563c48a
YY-GX <yyang9923@gmail.com>
2025-06-16 23:46:42 -0400

    Untrack all files and clean up repo

--- EgoVLPv2/data_loader/__init__.py
+++
@@ -1,6 +0,0 @@
-from .data_loader import *
-from .EgoClip_EgoMCQ_dataset import *
-from .EpicKitchens_MIR_dataset import *
-from .CharadesEgo_dataset import *
-from .Ego4D_MQ_dataset import *
-from .transforms import *
