9f2ef3c1d3385e29d621ba9bd9fa7f6b5ff38ff8
YY-GX <yyang9923@gmail.com>
2025-06-17 10:48:51 -0400

    update

--- EgoVLPv2/data_loader/ParkinsonEgo_dataset.py
+++ EgoVLPv2/data_loader/ParkinsonEgo_dataset.py
@@ -20,7 +20,6 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
 from base.base_dataset import TextVideoDataset
-from base.transforms import init_transform_dict, init_video_transform_dict
 
 
 class ParkinsonEgo(TextVideoDataset):
